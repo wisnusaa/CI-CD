7f25394fe4ed110ef9596500ab0c25e12abc4b30
wisnusaa <127575484+wisnusaa@users.noreply.github.com>
2025-05-07 20:20:47 +0700

    commit pertama

---
+++ Aritmatika.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a, b;
+	
+	cout << "input data : ";
+	cin >> a;
+	
+	cout << "input data : ";
+	cin >> b;
+	
+	cout << "hasil dari a + b : "<< a + b << endl;
+	cout << "hasil dari a - b : "<< a - b << endl;
+	cout << "hasil dari a * b : "<< a * b << endl;
+	cout << "hasil dari a / b : "<< a / b << endl;
+	
+	
+}
