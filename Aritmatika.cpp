06602774e3ca5918bd4da44ab72a2963d49387b4
eveeze <jsah737@gmail.com>
2025-05-08 08:47:02 +0700

    add c variabel and add the sum of a and c

--- Aritmatika.cpp
+++ Aritmatika.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 
 int main() {
-	float a, b;
+	float a, b, c;
 	
 	cout << "input data : ";
 	cin >> a;
 	
 	cout << "input data : ";
 	cin >> b;
+
+
+	cout << "input data c : ";
+	cin >> c;
 	
+
 	cout << "hasil dari a + b : "<< a + b << endl;
 	cout << "hasil dari a - b : "<< a - b << endl;
 	cout << "hasil dari a * b : "<< a * b << endl;
 	cout << "hasil dari a / b : "<< a / b << endl;
-	
-	
+  cout << "hasil dari a + c : "<< a + c << endl;
 }
