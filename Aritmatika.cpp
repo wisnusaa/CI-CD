afa279409ebeee3c8ce0be07c6cdd5eaa0398c8e
wisnusaa <127575484+wisnusaa@users.noreply.github.com>
2025-05-08 08:56:12 +0700

    tambah variabel c dan operasinya

--- Aritmatika.cpp
+++ Aritmatika.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	float a, b;
+	float a, b, c;
 	
 	cout << "input data : ";
 	cin >> a;
@@ -10,10 +10,13 @@ int main() {
 	cout << "input data : ";
 	cin >> b;
 	
+	cout << "input data c : ";
+	cin >> c;
+	
 	cout << "hasil dari a + b : "<< a + b << endl;
 	cout << "hasil dari a - b : "<< a - b << endl;
 	cout << "hasil dari a * b : "<< a * b << endl;
 	cout << "hasil dari a / b : "<< a / b << endl;
-	
+	cout << "hasil dari a - c : "<< a - c << endl;
 	
 }
